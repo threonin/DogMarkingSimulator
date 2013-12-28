1fc640fa31677ec47bc6616f82c1be827cf4c096
volker <volker@psoft.at>
2013-12-28 19:56:52 +0100

    noch mehr Zeug...

---
+++ src/mygame/Main.java
@@ -0,0 +1,50 @@
+package mygame;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Node;
+
+/**
+ * test
+ *
+ * @author normenhansen
+ */
+public class Main extends SimpleApplication {
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Node node = (Node) assetManager.loadModel("Scenes/HouseScene.j3o");
+        rootNode.attachChild(node);
+
+        /**
+         * A white, directional light source
+         */
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
+        sun.setColor(ColorRGBA.White);
+        rootNode.addLight(sun);
+
+        DirectionalLight sun2 = new DirectionalLight();
+        sun2.setDirection((new Vector3f(0.5f, 0.5f, 0.5f)).normalizeLocal());
+        sun2.setColor(ColorRGBA.White);
+        rootNode.addLight(sun2);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
+    }
+}
