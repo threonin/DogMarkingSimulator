69dba914a42ac769a489ab6b95ab7b9a4c7bf4fd
volker <volker@psoft.at>
2014-02-16 22:46:41 +0100

    Hundemarkierungssimulator angefangen.

---
+++ src/mygame/DogControl.java
@@ -0,0 +1,166 @@
+package mygame;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.AnimEventListener;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.control.BetterCharacterControl;
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.material.Material;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.control.AbstractControl;
+import com.jme3.scene.control.Control;
+import com.jme3.scene.shape.Cylinder;
+
+public class DogControl extends AbstractControl implements AnimEventListener {
+
+    private BulletAppState bulletAppState;
+    private BetterCharacterControl physicsCharacter;
+    private Node dog;
+    private Vector3f walkDirection = new Vector3f(0, 0, 0);
+    private Vector3f viewDirection = new Vector3f(0, 0, 1);
+    boolean forward = false, backward = false,
+            leftRotate = false, rightRotate = false;
+    private AnimChannel channel;
+    private ParticleEmitter pee;
+    private Material puddleMaterial;
+
+    public DogControl() {
+    }
+
+    public DogControl(BulletAppState bulletAppState, Material puddleMaterial) {
+        this.bulletAppState = bulletAppState;
+        this.puddleMaterial = puddleMaterial;
+    }
+
+    @Override
+    public Control cloneForSpatial(Spatial spatial) {
+        DogControl dogControl = new DogControl();
+        dogControl.setSpatial(spatial);
+        return dogControl;
+    }
+
+    @Override
+    public void setSpatial(Spatial spatial) {
+        super.setSpatial(spatial);
+        dog = (Node) spatial;
+        Node dognode = (Node) dog.getChild("Dog");
+        AnimControl anim = dognode.getControl(AnimControl.class);
+        pee = (ParticleEmitter) dog.getChild("Emitter");
+        anim.addListener(this);
+        channel = anim.createChannel();
+        channel.setAnim("idle");
+        physicsCharacter = dog.getControl(BetterCharacterControl.class);
+        if (physicsCharacter == null) {
+            physicsCharacter = new BetterCharacterControl(1f, 2f, 30f);
+            dog.addControl(physicsCharacter);
+        }
+        if (bulletAppState != null) {
+            bulletAppState.getPhysicsSpace().add(physicsCharacter);
+        }
+    }
+
+    @Override
+    protected void controlUpdate(float tpf) {
+        // Get current forward and left vectors of model by using its rotation
+        // to rotate the unit vectors
+        Vector3f modelForwardDir = dog.getWorldRotation().mult(Vector3f.UNIT_Z);
+
+        // WalkDirection is global!
+        // You *can* make your character fly with this.
+        walkDirection.set(0, 0, 0);
+        if (forward) {
+            walkDirection.addLocal(modelForwardDir.mult(3));
+        } else if (backward) {
+            walkDirection.addLocal(modelForwardDir.negate().multLocal(3));
+        }
+        physicsCharacter.setWalkDirection(walkDirection);
+
+        // ViewDirection is local to characters physics system!
+        // The final world rotation depends on the gravity and on the state of
+        // setApplyPhysicsLocal()
+        if (leftRotate) {
+            Quaternion rotateL = new Quaternion().fromAngleAxis(FastMath.PI * tpf, Vector3f.UNIT_Y);
+            rotateL.multLocal(viewDirection);
+        } else if (rightRotate) {
+            Quaternion rotateR = new Quaternion().fromAngleAxis(-FastMath.PI * tpf, Vector3f.UNIT_Y);
+            rotateR.multLocal(viewDirection);
+        }
+        physicsCharacter.setViewDirection(viewDirection);
+    }
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+    }
+
+    public void setForward(boolean forward) {
+        this.forward = forward;
+        if (forward) {
+            channel.setAnim("walk");
+            channel.setSpeed(2);
+        } else {
+            channel.setAnim("idle");
+        }
+    }
+
+    public void setBackward(boolean backward) {
+        this.backward = backward;
+        if (backward) {
+            channel.setAnim("walk");
+            channel.setSpeed(-2);
+        } else {
+            channel.setAnim("idle");
+        }
+    }
+
+    public void setLeftRotate(boolean leftRotate) {
+        this.leftRotate = leftRotate;
+    }
+
+    public void setRightRotate(boolean rightRotate) {
+        this.rightRotate = rightRotate;
+    }
+
+    public void mark() {
+        forward = false;
+        backward = false;
+        channel.setAnim("mark");
+        dog.attachChild(pee);
+        pee.setEnabled(true);
+    }
+
+    public void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName) {
+        if (animName.equals("mark")) {
+            channel.setAnim("idle");
+            pee.setEnabled(false);
+            pee.killAllParticles();
+            createPuddle();
+        }
+    }
+
+    private void createPuddle() {
+        Cylinder puddle = new Cylinder(4, 24, 0.3f, 0.025f, true);
+        Geometry puddleGeom = new Geometry("puddle", puddle);
+        puddleGeom.setLocalRotation(new Quaternion(new float[]{FastMath.PI / 2, 0, 0}));
+        Vector3f pos = dog.getLocalTranslation();
+        Vector3f puddlePos = new Vector3f(0.8f, 0, -0.25f);
+        dog.getLocalRotation().multLocal(puddlePos);
+        puddleGeom.setLocalTranslation(pos.x + puddlePos.x, pos.y + 0.0125f, pos.z + puddlePos.z);
+        puddleGeom.setMaterial(puddleMaterial);
+        dog.getParent().attachChild(puddleGeom);
+    }
+
+    public void onAnimChange(AnimControl control, AnimChannel channel, String animName) {
+        if (!animName.equals("mark")) {
+            pee.setEnabled(false);
+            pee.killAllParticles();
+        }
+    }
+}
--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,18 +1,26 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.input.ChaseCamera;
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Node;
 
-/**
- * test
- *
- * @author normenhansen
- */
-public class Main extends SimpleApplication {
+public class Main extends SimpleApplication implements ActionListener {
+
+    private Node dog;
+    private BulletAppState bulletAppState = new BulletAppState();
+    private DogControl dogControl;
+    private boolean skinchange;
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -21,8 +29,27 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
+        this.stateManager.attach(bulletAppState);
+        dogControl = new DogControl(bulletAppState, assetManager.loadMaterial("Materials/puddle.j3m"));
         Node node = (Node) assetManager.loadModel("Scenes/HouseScene2.j3o");
         rootNode.attachChild(node);
+        CollisionShape sceneShape =
+                CollisionShapeFactory.createMeshShape((Node) node);
+        RigidBodyControl sceneBody = new RigidBodyControl(sceneShape, 0);
+        node.addControl(sceneBody);
+        bulletAppState.getPhysicsSpace().addCollisionObject(sceneBody);
+
+        dog = (Node) assetManager.loadModel("Models/dog.j3o");
+        dog.setLocalTranslation(-10, 2f, 0);
+        rootNode.attachChild(dog);
+        dog.addControl(dogControl);
+
+        getFlyByCamera().setEnabled(false);
+        ChaseCamera chaseCam = new ChaseCamera(getCamera(), dog, getInputManager());
+        chaseCam.setSmoothMotion(true);
+        chaseCam.setMinDistance(3);
+        chaseCam.setDefaultDistance(5);
+        chaseCam.setMaxDistance(10);
 
         /**
          * A white, directional light source
@@ -36,16 +63,40 @@ public class Main extends SimpleApplication {
         sun2.setDirection((new Vector3f(0.5f, 0.5f, 0.5f)).normalizeLocal());
         sun2.setColor(ColorRGBA.White);
         rootNode.addLight(sun2);
-        this.getFlyByCamera().setMoveSpeed(30);
+        setupKeys(inputManager);
     }
 
-    @Override
-    public void simpleUpdate(float tpf) {
-        //TODO: add update code
+    private void setupKeys(InputManager inputManager) {
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_LEFT));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_RIGHT));
+        inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_UP));
+        inputManager.addMapping("Backward", new KeyTrigger(KeyInput.KEY_DOWN));
+        inputManager.addMapping("Mark", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addMapping("Change Skin", new KeyTrigger(KeyInput.KEY_TAB));
+        inputManager.addListener(this, "Left");
+        inputManager.addListener(this, "Right");
+        inputManager.addListener(this, "Forward");
+        inputManager.addListener(this, "Backward");
+        inputManager.addListener(this, "Mark");
+        inputManager.addListener(this, "Change Skin");
     }
 
-    @Override
-    public void simpleRender(RenderManager rm) {
-        //TODO: add render code
+    public void onAction(String binding, boolean value, float tpf) {
+        if (binding.equals("Left")) {
+            dogControl.setLeftRotate(value);
+        } else if (binding.equals("Right")) {
+            dogControl.setRightRotate(value);
+        } else if (binding.equals("Forward")) {
+            dogControl.setForward(value);
+        } else if (binding.equals("Backward")) {
+            dogControl.setBackward(value);
+        } else if (binding.equals("Mark")) {
+            dogControl.mark();
+        } else if (binding.equals("Change Skin")) {
+            if (value) {
+                ((Node) dog.getChild("Dog")).getChild(0).setMaterial(assetManager.loadMaterial(skinchange ? "Materials/Generated/dog-skin.j3m" : "Materials/Generated/dog-skin2.j3m"));
+                skinchange = !skinchange;
+            }
+        }
     }
 }
